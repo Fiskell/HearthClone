8530dde1362687b1eb6f262606affd5e4c05a880
Stephen Fiskell <stephen@stitchlabs.com>
2015-01-25 18:08:57 -0800

    Add test controller

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,8 @@ Route::get('/', 'WelcomeController@index');
 
 Route::get('home', 'HomeController@index');
 
+Route::get('test', 'TestController@index');
+
 Route::controllers([
 	'auth' => 'Auth\AuthController',
 	'password' => 'Auth\PasswordController',
