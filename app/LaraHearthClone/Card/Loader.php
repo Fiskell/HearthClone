e403966ab901c850df05dcc1511878058068f8cb
Stephen Fiskell <stephen@stitchlabs.com>
2015-01-28 23:03:52 -0800

    won't need a loader like this if i move it to db

---
+++ app/LaraHearthClone/Card/Loader.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: stephenfiskell
+ * Date: 1/28/15
+ * Time: 10:50 PM
+ */
+
+namespace LaraHearthClone\Card;
+
+class Loader
+{
+	public static function load($cardName)
+	{
+		//TODO cards need to be in db
+	}
+} 
