5f9c55bee35af042352a84803fa1cc70c74faba0
Fiskell <stephen.fiskell@gmail.com>
2015-08-23 18:51:48 -0700

    handle basic setting of attack and defense

--- app/Models/Card.php
+++ app/Models/Card.php
@@ -1,5 +1,6 @@
 <?php namespace App\Models;
 use App\Exceptions\MissingCardHandleException;
+use App\Exceptions\UnknownCardHandleException;
 
 /**
  * Created by PhpStorm.
@@ -9,9 +10,52 @@ use App\Exceptions\MissingCardHandleException;
  */
 class Card
 {
+    protected $handle;
+    protected $attack;
+    protected $defense;
+
     public function load($handle=null) {
-       if(is_null($handle)) {
+        if(is_null($handle)) {
            throw new MissingCardHandleException();
-       }
+        }
+        $this->handle = $handle;
+
+        switch($this->handle) {
+            case 'argent-squire':
+                $this->attack = 1;
+                $this->defense = 1;
+                break;
+            case 'knife-juggler':
+                $this->attack = 3;
+                $this->defense = 2;
+                break;
+            default:
+                throw new UnknownCardHandleException();
+        }
     }
+
+    /**
+     * @return null
+     */
+    public function getHandle()
+    {
+        return $this->handle;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAttack()
+    {
+        return $this->attack;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDefense()
+    {
+        return $this->defense;
+    }
+
 }
--- tests/CardTest.php
+++ tests/CardTest.php
@@ -8,10 +8,52 @@
  */
 class CardTest extends TestCase
 {
-    /** @expectedException \App\Exceptions\MissingCardHandleException
-     */
-    public function testCardLoadFailsWhenNoCardNameSpecified() {
-        $card = $this->app->make('Card');
-        $card->load();
+    public $card;
+
+    public function setUp() {
+        parent::setUp();
+        $this->card = $this->app->make('Card');
     }
+
+    /** @expectedException \App\Exceptions\MissingCardHandleException */
+    public function test_card_load_throws_when_no_card_name_specified() {
+        $this->card->load();
+    }
+
+    /** @expectedException \App\Exceptions\UnknownCardHandleException */
+    public function test_card_load_throws_when_unknown_handle_is_given() {
+        $this->card->load('NOT_A_REAL_CARD_HANDLE');
+    }
+
+    public function test_card_handle_is_set_when_handle_passed_into_load() {
+        $card_name = 'argent-squire';
+        $this->card->load($card_name);
+        $this->assertTrue($this->card->getHandle() == $card_name);
+    }
+
+    public function test_card_attack_is_set_on_load_argent_squire() {
+        $card_name = 'argent-squire';
+        $this->card->load($card_name);
+        $this->assertTrue($this->card->getAttack() == 1);
+    }
+
+    public function test_card_defense_is_set_on_load_argent_squire() {
+        $card_name = 'argent-squire';
+        $this->card->load($card_name);
+        $this->assertTrue($this->card->getDefense() == 1);
+    }
+
+    public function test_card_attack_is_set_on_load_knife_juggler() {
+        $card_name = 'knife-juggler';
+        $this->card->load($card_name);
+        $this->assertTrue($this->card->getAttack() == 3);
+    }
+
+    public function test_card_defense_is_set_on_load_knife_juggler() {
+        $card_name = 'knife-juggler';
+        $this->card->load($card_name);
+        $this->assertTrue($this->card->getDefense() == 2);
+    }
+
+
 }
