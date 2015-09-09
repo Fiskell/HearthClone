d3805e254f1cc1a6290ee45db4ba12403e611553
Fiskell <stephen.fiskell@gmail.com>
2015-09-08 23:54:42 -0700

    arcane shot works on hero and minion

--- tests/BasicSpellTest.php
+++ tests/BasicSpellTest.php
@@ -10,10 +10,15 @@ use App\Models\HearthCloneTest;
 class BasicSpellTest extends HearthCloneTest
 {
     /* Arcane Shot */
-    public function test_arcane_shot_does_two_damage_when_played() {
+    public function test_arcane_shot_does_two_damage_to_hero_when_played() {
         $player2 = $this->game->getPlayer2();
         $this->playCard('Arcane Shot', 1, [$player2->getHero()]);
         $this->assertEquals(28, $player2->getHero()->getHealth());
+    }
 
+    public function test_arcane_shot_does_two_damage_to_minion_when_played() {
+        $knife_juggler = $this->playCard('Knife Juggler', 1);
+        $this->playCard('Arcane Shot', 2, [$knife_juggler]);
+        $this->assertFalse($knife_juggler->isAlive());
     }
 }
