887fc48c53872326d078c80ac64a52cdd0272a80
Fiskell <stephen.fiskell@gmail.com>
2015-09-12 02:22:37 -0700

    Added wrath of air totem and tests

--- tests/BasicSpellpowerTest.php
+++ tests/BasicSpellpowerTest.php
@@ -14,11 +14,28 @@ class BasicSpellpowerTest extends HearthCloneTest
         $this->playCard('Archmage', 1);
         $this->assertTrue($this->game->getPlayer1()->getSpellPowerModifier() == 1);
     }
-    
+
+    /* Dalaran Mage */
+    public function test_dalaran_mage_increases_spell_power_by_one() {
+        $this->playCard('Dalaran Mage', 1);
+        $this->assertTrue($this->game->getPlayer1()->getSpellPowerModifier() == 1);
+    }
+
+    /* Kobold Geomancer */
+    public function test_kobold_geomancer_increases_spell_power_by_one() {
+        $this->playCard('Kobold Geomancer', 1);
+        $this->assertTrue($this->game->getPlayer1()->getSpellPowerModifier() == 1);
+    }
 
     /* Ogre Magi */
     public function test_ogre_magi_increases_spell_power_by_one() {
         $this->playCard('Ogre Magi', 1);
+        $this->assertTrue($this->game->getPlayer1()->getSpellPowerModifier() == 1);
+    }
+
+    /* Wrath of Air Totem */
+    public function test_wrath_of_air_totem_increases_spell_power_by_one() {
+        $this->playCard('Wrath of Air Totem', 1);
         $this->assertTrue($this->game->getPlayer1()->getSpellPowerModifier() == 1);
     }
 }
