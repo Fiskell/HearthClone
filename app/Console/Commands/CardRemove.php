ceea4f21bbeac7f704fd868c0ecf8420f261c1f8
Fiskell <stephen.fiskell@gmail.com>
2015-09-11 23:32:59 -0700

    shell for remove card command

---
+++ app/Console/Commands/CardRemove.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kegimaro
+ * Date: 9/11/15
+ * Time: 11:31 PM
+ */
+
+namespace Console\Commands;
+
+use Illuminate\Console\Command;
+
+class CardRemove extends Command
+{
+    public function handle() {
+        
+    }
+}
