d088ae60550b8049136b7fa0476100f38822e750
Fiskell <stephen.fiskell@gmail.com>
2015-09-12 20:32:03 -0700

    simplify phaseResolve code so I can reuse it in a minute

--- app/Game/Sequences/Phases/CardPhase.php
+++ app/Game/Sequences/Phases/CardPhase.php
@@ -14,9 +14,9 @@ abstract class CardPhase extends AbstractPhase
     /** @var Minion $minion */
     protected $card;
     protected $targets;
-    public $phase_name;
+    public    $phase_name;
 
-    abstract function queue(Minion $minion, array $targets=[]);
+    abstract function queue(Minion $minion, array $targets = []);
 
     public function resolve() {
 
@@ -26,7 +26,7 @@ abstract class CardPhase extends AbstractPhase
             $trigger = array_get($this->card->getTrigger(), TriggerTypes::$CHOOSE_ONE . '.' . ($this->card->getChooseOption() - 1));
         }
 
-        if(array_get($this->card->getTrigger(), TriggerTypes::$OVERLOAD)) {
+        if (array_get($this->card->getTrigger(), TriggerTypes::$OVERLOAD)) {
             $overload_value = array_get($this->card->getTrigger(), TriggerTypes::$OVERLOAD);
             $this->card->getOwner()->addLockedManaCrystalCount($overload_value);
         }
@@ -35,152 +35,50 @@ abstract class CardPhase extends AbstractPhase
             throw new DumbassDeveloperException('Trigger not specified for ' . $this->card->getName());
         }
 
-        $targets = [];
-        if (array_get($trigger, 'targets')) {
-            $target_type = array_get($trigger, 'targets.type');
-            if (is_null($target_type)) {
-                throw new DumbassDeveloperException('Missing target type for ' . $this->card->getName());
-            }
-
-            $targets = $this->getTargets($this->card, $target_type);
-        }
+        /* Get Targets */
+        $targets = $this->getTargets($this->card, $trigger);
 
         /* Check if race is correct */
-        $required_race = array_get($trigger, 'targets.race');
-        if ($required_race) {
-            /** @var Minion $target */
-            foreach ($targets as $target) {
-                if (strtolower($target->getRace()) != strtolower($required_race)) {
-                    throw new InvalidTargetException('Target must be a ' . $required_race . ' ' . $target->getRace() . ' given');
-                }
-            }
-        }
+        $this->validateRace($trigger, $targets);
 
         /* Destroy */
-        $destroy = array_get($trigger, 'destroy');
-        if (!is_null($destroy)) {
-            /** @var AbstractHero $target */
-            foreach ($targets as $target) {
-                // todo may need to have other types of things to destroy.
-                $target->destroyWeapon();
-            }
-        }
+        $this->resolveDestroyTrigger($trigger, $targets);
 
         /* Silence */
-        $silence = array_get($trigger, 'silence');
-        if (!is_null($silence)) {
-            foreach ($targets as $target) {
-                $target->removeAllMechanics();
-            }
-        }
+        $this->resolveSilenceTrigger($trigger, $targets);
 
         /* Spell */
-        $spell = array_get($trigger, 'spell');
-        if (!is_null($spell)) {
-            foreach ($targets as $target) {
-                $target->setMechanics(array_get($spell, 'mechanics', []));
-
-                $delta_attack = array_get($spell, 'attack', 0);
-                $target->setAttack($target->getAttack() + $delta_attack);
-
-                $delta_health = array_get($spell, 'health', 0);
-                $target->setHealth($target->getHealth() + $delta_health);
-
-                $full_health = array_get($spell, 'full_health');
-                if ($full_health) {
-                    $target->setHealth($target->getMaxHealth());
-                }
-            }
-
-            $attack_by_count = array_get($spell, 'attack_by_count');
-            if (!is_null($attack_by_count)) {
-                $delta_attack = count($this->getTargets($this->card, $attack_by_count));
-                $this->card->setAttack($this->card->getAttack() + $delta_attack);
-            }
-
-            $health_by_count = array_get($spell, 'health_by_count');
-            if (!is_null($health_by_count)) {
-                $delta_health = count($this->getTargets($this->card, $health_by_count));
-                $this->card->setHealth($this->card->getHealth() + $delta_health);
-            }
-        }
+        $this->resolveSpellTrigger($trigger, $targets);
 
         /* Enchantment */
-        $enchantment = array_get($trigger, 'enchantment');
-        if (!is_null($enchantment)) {
-            foreach ($targets as $target) {
-                $target->setMechanics(array_get($enchantment, 'mechanics', []));
-
-                $delta_attack = array_get($enchantment, 'attack', 0);
-                $target->setAttack($target->getAttack() + $delta_attack);
-
-                $delta_health = array_get($enchantment, 'health', 0);
-                $target->setMaxHealth($target->getHealth() + $delta_health);
-                $target->setHealth($target->getHealth() + $delta_health);
-            }
-
-            $attack_by_count = array_get($enchantment, 'attack_by_count');
-            if (!is_null($attack_by_count)) {
-                $delta_attack = count($this->getTargets($this->card, $attack_by_count));
-                $this->card->setAttack($this->card->getAttack() + $delta_attack);
-            }
-
-            $health_by_count = array_get($enchantment, 'health_by_count');
-            if (!is_null($health_by_count)) {
-                $delta_health = count($this->getTargets($this->card, $health_by_count));
-                $this->card->setMaxHealth($this->card->getHealth() + $delta_health);
-                $this->card->setHealth($this->card->getHealth() + $delta_health);
-            }
-        }
+        $this->resolveEnchantmentTrigger($trigger, $targets);
 
         /* Discard */
-        $discard = array_get($trigger, 'discard');
-        if (!is_null($discard)) {
-            /** @var Player $target */
-            foreach ($targets as $target) {
-                $type     = array_get($discard, 'type');
-                $quantity = array_get($discard, 'quantity');
-                switch ($type) {
-                    case 'random':
-                        $target->discardRandom($quantity);
-                        break;
-                    default:
-                        throw new DumbassDeveloperException('Unknown discard type ' . $type);
-                }
-            }
-        }
+        $this->resolveDiscardTrigger($trigger, $targets);
 
         /* Draw */
-        $draw = array_get($trigger, 'draw');
-        if (!is_null($draw)) {
-            /** @var Player $target */
-            foreach ($targets as $target) {
-                for ($i = 0; $i < $draw; $i++) {
-                    $target->drawCard();
-                }
-            }
-        }
+        $this->resolveDrawTrigger($trigger, $targets);
 
         /* Summon */
-        $summon_name     = array_get($trigger, 'summon.name');
-        $summon_quantity = array_get($trigger, 'summon.quantity');
-        if (!is_null($summon_name)) {
-            /** @var Player $target */
-            for ($i = 0; $i < $summon_quantity; $i++) {
-                $tmp_minion = app('Minion', [$this->card->getOwner()]);
-                $tmp_minion->load($summon_name);
-                $this->card->getOwner()->play($tmp_minion);
-            }
-        }
+        $this->resolveSummonTrigger($trigger);
     }
 
     /**
      * @param Card $trigger_card
-     * @param $target_type
+     * @param $trigger
      * @return array
      * @throws DumbassDeveloperException
      */
-    private function getTargets(Card $trigger_card, $target_type) {
+    private function getTargets(Card $trigger_card, $trigger) {
+        $trigger_targets = array_get($trigger, 'targets');
+        if (!$trigger_targets) {
+            return [];
+        }
+
+        $target_type = array_get($trigger, 'targets.type');
+        if (is_null($target_type)) {
+            throw new DumbassDeveloperException('Missing target type for ' . $this->card->getName());
+        }
 
         $player           = $trigger_card->getOwner();
         $player_minions   = $player->getMinionsInPlay();
@@ -229,6 +127,201 @@ abstract class CardPhase extends AbstractPhase
         }
 
         return $targets;
+    }
+
+    /**
+     * @param $trigger
+     * @param $targets
+     * @return int
+     */
+    private function resolveDrawTrigger($trigger, $targets) {
+        $draw = array_get($trigger, 'draw');
+
+        if (is_null($draw)) {
+            return;
+        }
+
+        /** @var Player $target */
+        foreach ($targets as $target) {
+            for ($i = 0; $i < $draw; $i++) {
+                $target->drawCard();
+            }
+        }
+    }
+
+    /**
+     * @param $trigger
+     */
+    private function resolveSummonTrigger($trigger) {
+        $summon_name     = array_get($trigger, 'summon.name');
+        $summon_quantity = array_get($trigger, 'summon.quantity');
+
+        if (is_null($summon_name)) {
+            return;
+        }
+
+        /** @var Player $target */
+        for ($i = 0; $i < $summon_quantity; $i++) {
+            $tmp_minion = app('Minion', [$this->card->getOwner()]);
+            $tmp_minion->load($summon_name);
+            $this->card->getOwner()->play($tmp_minion);
+        }
+    }
+
+    /**
+     * @param $trigger
+     * @param $targets
+     * @throws DumbassDeveloperException
+     */
+    private function resolveDiscardTrigger($trigger, $targets) {
+        $discard = array_get($trigger, 'discard');
+
+        if (is_null($discard)) {
+            return;
+        }
+
+        /** @var Player $target */
+        foreach ($targets as $target) {
+            $type     = array_get($discard, 'type');
+            $quantity = array_get($discard, 'quantity');
+            switch ($type) {
+                case 'random':
+                    $target->discardRandom($quantity);
+                    break;
+                default:
+                    throw new DumbassDeveloperException('Unknown discard type ' . $type);
+            }
+        }
+    }
+
+    /**
+     * @param $trigger
+     * @param Minion[] $targets
+     * @throws DumbassDeveloperException
+     */
+    private function resolveEnchantmentTrigger($trigger, $targets) {
+        $enchantment = array_get($trigger, 'enchantment');
+        if (is_null($enchantment)) {
+            return;
+        }
+
+        foreach ($targets as $target) {
+            $target->setMechanics(array_get($enchantment, 'mechanics', []));
+
+            $delta_attack = array_get($enchantment, 'attack', 0);
+            $target->setAttack($target->getAttack() + $delta_attack);
+
+            $delta_health = array_get($enchantment, 'health', 0);
+            $target->setMaxHealth($target->getHealth() + $delta_health);
+            $target->setHealth($target->getHealth() + $delta_health);
+        }
+
+        $attack_by_count = array_get($enchantment, 'attack_by_count');
+        if (!is_null($attack_by_count)) {
+            $delta_attack = count($this->getTargets($this->card, $attack_by_count));
+            $this->card->setAttack($this->card->getAttack() + $delta_attack);
+        }
+
+        $health_by_count = array_get($enchantment, 'health_by_count');
+        if (!is_null($health_by_count)) {
+            $delta_health = count($this->getTargets($this->card, $health_by_count));
+            $this->card->setMaxHealth($this->card->getHealth() + $delta_health);
+            $this->card->setHealth($this->card->getHealth() + $delta_health);
+        }
+    }
+
+    /**
+     * @param $trigger
+     * @param Minion[] $targets
+     * @throws DumbassDeveloperException
+     */
+    private function resolveSpellTrigger($trigger, $targets) {
+        $spell = array_get($trigger, 'spell');
+
+        if (is_null($spell)) {
+            return;
+        }
+
+        foreach ($targets as $target) {
+            $target->setMechanics(array_get($spell, 'mechanics', []));
+
+            $delta_attack = array_get($spell, 'attack', 0);
+            $target->setAttack($target->getAttack() + $delta_attack);
+
+            $delta_health = array_get($spell, 'health', 0);
+            $target->setHealth($target->getHealth() + $delta_health);
+
+            $full_health = array_get($spell, 'full_health');
+            if ($full_health) {
+                $target->setHealth($target->getMaxHealth());
+            }
+        }
+
+        $attack_by_count = array_get($spell, 'attack_by_count');
+        if (!is_null($attack_by_count)) {
+            $delta_attack = count($this->getTargets($this->card, $attack_by_count));
+            $this->card->setAttack($this->card->getAttack() + $delta_attack);
+        }
+
+        $health_by_count = array_get($spell, 'health_by_count');
+        if (!is_null($health_by_count)) {
+            $delta_health = count($this->getTargets($this->card, $health_by_count));
+            $this->card->setHealth($this->card->getHealth() + $delta_health);
+        }
+    }
+
+    /**
+     * @param $trigger
+     * @param Minion[] $targets
+     */
+    private function resolveSilenceTrigger($trigger, $targets) {
+        $silence = array_get($trigger, 'silence');
+
+        if (is_null($silence)) {
+            return;
+        }
+
+        foreach ($targets as $target) {
+            $target->removeAllMechanics();
+        }
+    }
+
+    /**
+     * @param $trigger
+     * @param $targets
+     */
+    private function resolveDestroyTrigger($trigger, $targets) {
+        $destroy = array_get($trigger, 'destroy');
+
+        if (is_null($destroy)) {
+            return;
+        }
+
+        /** @var AbstractHero $target */
+        foreach ($targets as $target) {
+            // todo may need to have other types of things to destroy.
+            $target->destroyWeapon();
+        }
+    }
+
+    /**
+     * @param $trigger
+     * @param $targets
+     * @throws InvalidTargetException
+     */
+    private function validateRace($trigger, $targets) {
+        $required_race = array_get($trigger, 'targets.race');
+
+        if (!$required_race) {
+            return;
+        }
+
+        /** @var Minion $target */
+        foreach ($targets as $target) {
+            if (strtolower($target->getRace()) != strtolower($required_race)) {
+                throw new InvalidTargetException('Target must be a ' . $required_race . ' ' . $target->getRace() . ' given');
+            }
+        }
     }
 
 }
