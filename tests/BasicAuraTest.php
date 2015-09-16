54f9cfb05046001987158e4e4e2e77094b73ea80
Fiskell <stephen.fiskell@gmail.com>
2015-09-15 22:59:51 -0700

    add Raid Leader test

--- tests/BasicAuraTest.php
+++ tests/BasicAuraTest.php
@@ -4,10 +4,26 @@ use App\Models\HearthCloneTest;
 
 class BasicAuraTest extends HearthCloneTest
 {
+    /* Leokk */
     public function test_leokk_gives_two_friendly_minions_one_attack() {
         $wisp = $this->playCard('Wisp', 1);
 
         $this->playCard('Leokk', 1);
+        $this->assertEquals(2, $wisp->getAttack());
+
+        $wisp2 = $this->playCard('Wisp', 1);
+
+        // Make sure wisp 1 is still buffed
+        $this->assertEquals(2, $wisp->getAttack());
+
+        $this->assertEquals(2, $wisp2->getAttack());
+    }
+
+    /* Raid Leader */
+    public function test_raid_leader_gives_two_friendly_minions_one_attack() {
+        $wisp = $this->playCard('Wisp', 1);
+
+        $this->playCard('Raid Leader', 1);
         $this->assertEquals(2, $wisp->getAttack());
 
         $wisp2 = $this->playCard('Wisp', 1);
