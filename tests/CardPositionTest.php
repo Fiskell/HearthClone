be82839cfc6c778a7346c48a773df650c3b7a4b7
Fiskell <stephen.fiskell@gmail.com>
2015-09-17 03:48:31 -0700

    Position resets on both sides of the board on death processing

--- tests/CardPositionTest.php
+++ tests/CardPositionTest.php
@@ -19,18 +19,20 @@ class CardPositionTest extends HearthCloneTest
     }
 
     public function test_first_minion_dies_moves_second_position_minion_to_position_1() {
-        $attacking_wisp = $this->playCard('Wisp', 2);
+        $wisp_player2_1 = $this->playCard('Wisp', 2, [], false, null, 1);
+        $wisp_player2_2 = $this->playCard('Wisp', 2, [], false, null, 2);
 
-        $wisp = $this->playCard('Wisp', 1, [], false, null, 1);
-        $this->assertEquals(1, $wisp->getPosition());
-        $wisp2 = $this->playCard('Wisp', 1, [], false, null, 2);
+        $wisp_player1_1 = $this->playCard('Wisp', 1, [], false, null, 1);
+        $this->assertEquals(1, $wisp_player1_1->getPosition());
+        $wisp_player1_2 = $this->playCard('Wisp', 1, [], false, null, 2);
 
-        $this->assertEquals(1, $wisp->getPosition());
-        $this->assertEquals(2, $wisp2->getPosition());
+        $this->assertEquals(1, $wisp_player1_1->getPosition());
+        $this->assertEquals(2, $wisp_player1_2->getPosition());
 
-        $attacking_wisp->attack($wisp);
+        $wisp_player2_1->attack($wisp_player1_1);
 
-        $this->assertEquals(1, $wisp2->getPosition());
+        $this->assertEquals(1, $wisp_player1_2->getPosition());
+        $this->assertEquals(1, $wisp_player2_2->getPosition());
     }
 
 }
