b5928a5647f30649fda270857a7b28129a35cd2f
Fiskell <stephen.fiskell@gmail.com>
2015-09-23 21:13:28 -0700

    Add undamaged_provided_minion target type

--- app/Game/Cards/Triggers/TargetTypes.php
+++ app/Game/Cards/Triggers/TargetTypes.php
@@ -8,6 +8,8 @@
  */
 class TargetTypes
 {
+    public static $PROVIDED_MINION                  = 'provided_minion';
+    public static $UNDAMAGED_PROVIDED_MINION        = 'undamaged_provided_minion';
     public static $RANDOM_OPPONENT_CHARACTER        = 'random_opponent_character';
     public static $ALL_FRIENDLY_CHARACTERS          = 'all_friendly_characters';
     public static $ALL_FRIENDLY_MINIONS             = 'all_friendly_minions';
@@ -16,7 +18,6 @@ class TargetTypes
     public static $ALL_OPPONENT_MINIONS             = 'all_opponent_minions';
     public static $OTHER_FRIENDLY_MINIONS_WITH_RACE = 'other_friendly_minions_with_race';
     public static $OTHER_FRIENDLY_MINIONS           = 'other_friendly_minions';
-    public static $PROVIDED_MINION                  = 'provided_minion';
     public static $FRIENDLY_PLAYER                  = 'friendly_player';
     public static $FRIENDLY_HERO                    = 'friendly_hero';
     public static $OPPONENT_HERO                    = 'opponent_hero';
