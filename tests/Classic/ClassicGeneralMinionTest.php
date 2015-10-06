3fbcaac456c5026aa3e65c9c8ce337d3547438a8
Fiskell <stephen.fiskell@gmail.com>
2015-10-05 22:31:33 -0700

    Add todo for all classic minions, credit to tuccinator!

--- tests/Classic/ClassicGeneralMinionTest.php
+++ tests/Classic/ClassicGeneralMinionTest.php
@@ -5,11 +5,1163 @@ use App\Models\HearthCloneTest;
 
 class ClassicGeneralMinionTest extends HearthCloneTest
 {
+    /* 'Inspired' */
+    // todo
+    
+    /* Abomination */
+    // todo
+
+    /* Abusive Sergeant */
+    // todo
+
+    /* Acolyte of Pain */
+    // todo
+
+    /* Al'Akir the Windlord */
+    // todo
+
+    /* Alarm-o-Bot */
+    // todo
+
+    /* Aldor Peacekeeper */
+    // todo
+
+    /* Alexstrasza */
+    // todo
+
+    /* Alexstrasza's Fire */
+    // todo
+
+    /* Amani Berserker */
+    // todo
+
+    /* Ancestral Spirit */
+    // todo
+
+    /* Ancestral Spirit */
+    // todo
+
+    /* Ancient Brewmaster */
+    // todo
+
+    /* Ancient Mage */
+    // todo
+
+    /* Ancient Secrets */
+    // todo
+
+    /* Ancient Teachings */
+    // todo
+
+    /* Ancient Watcher */
+    // todo
+
+    /* Ancient of Lore */
+    // todo
+
+    /* Ancient of War */
+    // todo
+
+    /* Angry Chicken */
+    // todo
+
+    /* Arathi Weaponsmith */
+    // todo
+
+    /* Arcane Golem */
+    // todo
+
+    /* Archmage Antonidas */
+    // todo
+
+    /* Argent Commander */
+    // todo
+
+    /* Argent Protector */
+    // todo
+
+    /* Argent Squire */
+    // todo
+
+    /* Armorsmith */
+    // todo
+
+    /* Ashbringer */
+    // todo
+
+    /* Auchenai Soulpriest */
+    // todo
+
+    /* Avenging Wrath */
+    // todo
+
+    /* Azure Drake */
+    // todo
+
+    /* Baine Bloodhoof */
+    // todo
+
+    /* Bananas */
+    // todo
+
+    /* Bananas */
+    // todo
+
+    /* Bane of Doom */
+    // todo
+
+    /* Baron Geddon */
+    // todo
+
+    /* Battle Axe */
+    // todo
+
+    /* Battle Rage */
+    // todo
+
+    /* Bear Form */
+    // todo
+
+    /* Berserk */
+    // todo
+
+    /* Bestial Wrath */
+    // todo
+
+    /* Bestial Wrath */
+    // todo
+
+    /* Betrayal */
+    // todo
+
+    /* Big Game Hunter */
+    // todo
+
+    /* Bite */
+    // todo
+
+    /* Bite */
+    // todo
+
+    /* Blade Flurry */
+    // todo
+
+    /* Blarghghl */
+    // todo
+
+    /* Blessed Champion */
+    // todo
+
+    /* Blessed Champion */
+    // todo
+
+    /* Blessing of Wisdom */
+    // todo
+
+    /* Blessing of Wisdom */
+    // todo
+
+    /* Blessing of Wisdom */
+    // todo
+
+    /* Blizzard */
+    // todo
+
+    /* Blood Fury */
+    // todo
+
+    /* Blood Imp */
+    // todo
+
+    /* Blood Knight */
+    // todo
+
+    /* Blood Pact */
+    // todo
+
+    /* Bloodmage Thalnos */
+    // todo
+
+    /* Bloodrage */
+    // todo
+
+    /* Bloodsail Corsair */
+    // todo
+
+    /* Bloodsail Raider */
+    // todo
+
+    /* Bolstered */
+    // todo
+
+    /* Brawl */
+    // todo
+
+    /* Cabal Shadow Priest */
+    // todo
+
+    /* Cairne Bloodhoof */
+    // todo
+
+    /* Cannibalize */
+    // todo
+
+    /* Captain Greenskin */
+    // todo
+
+    /* Cat Form */
+    // todo
+
+    /* Cenarius */
+    // todo
+
+    /* Circle of Healing */
+    // todo
+
+    /* Cold Blood */
+    // todo
+
+    /* Cold Blood */
+    // todo
+
+    /* Cold Blood */
+    // todo
+
+    /* Coldlight Oracle */
+    // todo
+
+    /* Coldlight Seer */
+    // todo
+
+    /* Commanding Shout */
+    // todo
+
+    /* Commanding Shout */
+    // todo
+
+    /* Commanding Shout */
+    // todo
+
+    /* Conceal */
+    // todo
+
+    /* Concealed */
+    // todo
+
+    /* Cone of Cold */
+    // todo
+
+    /* Consume */
+    // todo
+
+    /* Counterspell */
+    // todo
+
+    /* Crazed Alchemist */
+    // todo
+
+    /* Cruel Taskmaster */
+    // todo
+
+    /* Cult Master */
+    // todo
+
+    /* Damaged Golem */
+    // todo
+
+    /* Dark Iron Dwarf */
+    // todo
+
+    /* Deadly Shot */
+    // todo
+
+    /* Deathwing */
+    // todo
+
+    /* Defender */
+    // todo
+
+    /* Defender of Argus */
+    // todo
+
+    /* Defias Bandit */
+    // todo
+
+    /* Defias Ringleader */
+    // todo
+
+    /* Demigod's Favor */
+    // todo
+
+    /* Demigod's Favor */
+    // todo
+
+    /* Demolisher */
+    // todo
+
+    /* Demonfire */
+    // todo
+
+    /* Demonfire */
+    // todo
+
+    /* Devilsaur */
+    // todo
+
+    /* Dire Wolf Alpha */
+    // todo
+
+    /* Dispel */
+    // todo
+
+    /* Divine Favor */
+    // todo
+
+    /* Doomguard */
+    // todo
+
+    /* Doomhammer */
+    // todo
+
+    /* Doomsayer */
+    // todo
+
+    /* Dread Corsair */
+    // todo
+
+    /* Dream */
+    // todo
+
+    /* Druid of the Claw */
+    // todo
+
+    /* Druid of the Claw */
+    // todo
+
+    /* Druid of the Claw */
+    // todo
+
+    /* Dust Devil */
+    // todo
+
+    /* Eaglehorn Bow */
+    // todo
+
+    /* Earth Elemental */
+    // todo
+
+    /* Earth Shock */
+    // todo
+
+    /* Earthen Ring Farseer */
+    // todo
+
+    /* Edwin VanCleef */
+    // todo
+
+    /* Elune's Grace */
+    // todo
+
+    /* Emerald Drake */
+    // todo
+
+    /* Emperor Cobra */
+    // todo
+
+    /* Empowered */
+    // todo
+
+    /* Equality */
+    // todo
+
+    /* Equality */
+    // todo
+
+    /* Equipped */
+    // todo
+
+    /* Ethereal Arcanist */
+    // todo
+
+    /* Eviscerate */
+    // todo
+
+    /* Experiments! */
+    // todo
+
+    /* Explosive Shot */
+    // todo
+
+    /* Explosive Trap */
+    // todo
+
+    /* Eye for an Eye */
+    // todo
+
+    /* Faceless Manipulator */
+    // todo
+
+    /* Faerie Dragon */
+    // todo
+
+    /* Far Sight */
+    // todo
+
+    /* Far Sight */
+    // todo
+
+    /* Felguard */
+    // todo
+
+    /* Fen Creeper */
+    // todo
+
+    /* Feral Spirit */
+    // todo
+
+    /* Finkle Einhorn */
+    // todo
+
+    /* Flame Imp */
+    // todo
+
+    /* Flame of Azzinoth */
+    // todo
+
+    /* Flare */
+    // todo
+
+    /* Flesheating Ghoul */
+    // todo
+
+    /* Force of Nature */
+    // todo
+
+    /* Forked Lightning */
+    // todo
+
+    /* Freezing Trap */
+    // todo
+
+    /* Frost Elemental */
+    // todo
+
+    /* Frothing Berserker */
+    // todo
+
+    /* Full Belly */
+    // todo
+
+    /* Full Strength */
+    // todo
+
+    /* Gadgetzan Auctioneer */
+    // todo
+
+    /* Gladiator's Longbow */
+    // todo
+
+    /* Gnoll */
+    // todo
+
+    /* Gorehowl */
+    // todo
+
+    /* Greenskin's Command */
+    // todo
+
+    /* Grommash Hellscream */
+    // todo
+
+    /* Growth */
+    // todo
+
+    /* Gruul */
+    // todo
+
+    /* Hand of Argus */
+    // todo
+
+    /* Harrison Jones */
+    // todo
+
+    /* Harvest Golem */
+    // todo
+
+    /* Headcrack */
+    // todo
+
+    /* Heavy Axe */
+    // todo
+
+    /* Hogger */
+    // todo
+
+    /* Holy Fire */
+    // todo
+
+    /* Holy Wrath */
+    // todo
+
+    /* Hound */
+    // todo
+
+    /* Hour of Twilight */
+    // todo
+
+    /* Hungry Crab */
+    // todo
+
+    /* Hyena */
+    // todo
+
+    /* INFERNO! */
+    // todo
+
+    /* Ice Barrier */
+    // todo
+
+    /* Ice Block */
+    // todo
+
+    /* Ice Block */
+    // todo
+
+    /* Ice Lance */
+    // todo
+
+    /* Illidan Stormrage */
+    // todo
+
+    /* Imp */
+    // todo
+
+    /* Imp Master */
+    // todo
+
+    /* Infernal */
+    // todo
+
+    /* Infusion */
+    // todo
+
+    /* Injured Blademaster */
+    // todo
+
+    /* Inner Fire */
+    // todo
+
+    /* Inner Fire */
+    // todo
+
+    /* Inner Rage */
+    // todo
+
+    /* Inner Rage */
+    // todo
+
     /* Ironbeak Owl */
     public function test_ironbeak_owl_silences_minion() {
         $frostwolf_grunt = $this->playCard("Frostwolf Grunt", 1);
         $this->playCard("Ironbeak Owl", 2, [$frostwolf_grunt]);
         $this->assertFalse($frostwolf_grunt->hasMechanic(Mechanics::$TAUNT));
     }
+
+    /* Jungle Panther */
+    // todo
+
+    /* Justice Served */
+    // todo
+
+    /* Keeper of the Grove */
+    // todo
+
+    /* Keeping Secrets */
+    // todo
+
+    /* Kidnapper */
+    // todo
+
+    /* Kill Millhouse! */
+    // todo
+
+    /* King Krush */
+    // todo
+
+    /* King Mukla */
+    // todo
+
+    /* Kirin Tor Mage */
+    // todo
+
+    /* Knife Juggler */
+    // todo
+
+    /* Laughing Sister */
+    // todo
+
+    /* Lava Burst */
+    // todo
+
+    /* Lay on Hands */
+    // todo
+
+    /* Leader of the Pack */
+    // todo
+
+    /* Leader of the Pack */
+    // todo
+
+    /* Leeroy Jenkins */
+    // todo
+
+    /* Leper Gnome */
+    // todo
+
+    /* Level Up! */
+    // todo
+
+    /* Lightning Bolt */
+    // todo
+
+    /* Lightning Storm */
+    // todo
+
+    /* Lightspawn */
+    // todo
+
+    /* Lightwarden */
+    // todo
+
+    /* Lightwell */
+    // todo
+
+    /* Loot Hoarder */
+    // todo
+
+    /* Lord Jaraxxus */
+    // todo
+
+    /* Lord Jaraxxus */
+    // todo
+
+    /* Lorewalker Cho */
+    // todo
+
+    /* Mad Bomber */
+    // todo
+
+    /* Malygos */
+    // todo
+
+    /* Mana Addict */
+    // todo
+
+    /* Mana Gorged */
+    // todo
+
+    /* Mana Tide Totem */
+    // todo
+
+    /* Mana Wraith */
+    // todo
+
+    /* Mana Wyrm */
+    // todo
+
+    /* Mark of Nature */
+    // todo
+
+    /* Mark of Nature */
+    // todo
+
+    /* Mark of Nature */
+    // todo
+
+    /* Mark of Nature */
+    // todo
+
+    /* Mark of Nature */
+    // todo
+
+    /* Mass Dispel */
+    // todo
+
+    /* Master Swordsmith */
+    // todo
+
+    /* Master of Disguise */
+    // todo
+
+    /* Millhouse Manastorm */
+    // todo
+
+    /* Mind Control Tech */
+    // todo
+
+    /* Mind Controlling */
+    // todo
+
+    /* Mind Shatter */
+    // todo
+
+    /* Mind Spike */
+    // todo
+
+    /* Mindgames */
+    // todo
+
+    /* Mirror Entity */
+    // todo
+
+    /* Misdirection */
+    // todo
+
+    /* Mogu'shan Warden */
+    // todo
+
+    /* Molten Giant */
+    // todo
+
+    /* Moonfire */
+    // todo
+
+    /* Mortal Strike */
+    // todo
+
+    /* Mountain Giant */
+    // todo
+
+    /* Mrgglaargl! */
+    // todo
+
+    /* Mrghlglhal */
+    // todo
+
+    /* Murloc Tidecaller */
+    // todo
+
+    /* Murloc Warleader */
+    // todo
+
+    /* Nat Pagle */
+    // todo
+
+    /* Naturalize */
+    // todo
+
+    /* Needs Sharpening */
+    // todo
+
+    /* Nightmare */
+    // todo
+
+    /* Nightmare */
+    // todo
+
+    /* Noble Sacrifice */
+    // todo
+
+    /* Nourish */
+    // todo
+
+    /* Nourish */
+    // todo
+
+    /* Nourish */
+    // todo
+
+    /* Nozdormu */
+    // todo
+
+    /* Onyxia */
+    // todo
+
+    /* Overloading */
+    // todo
+
+    /* Panther */
+    // todo
+
+    /* Patient Assassin */
+    // todo
+
+    /* Perdition's Blade */
+    // todo
+
+    /* Pint-Sized Summoner */
+    // todo
+
+    /* Pit Lord */
+    // todo
+
+    /* Power Overwhelming */
+    // todo
+
+    /* Power Overwhelming */
+    // todo
+
+    /* Power of the Kirin Tor */
+    // todo
+
+    /* Power of the Wild */
+    // todo
+
+    /* Preparation */
+    // todo
+
+    /* Preparation */
+    // todo
+
+    /* Priestess of Elune */
+    // todo
+
+    /* Prophet Velen */
+    // todo
+
+    /* Pyroblast */
+    // todo
+
+    /* Questing Adventurer */
+    // todo
+
+    /* Raging Worgen */
+    // todo
+
+    /* Ragnaros the Firelord */
+    // todo
+
+    /* Rampage */
+    // todo
+
+    /* Rampage */
+    // todo
+
+    /* Ravenholdt Assassin */
+    // todo
+
+    /* Raw Power! */
+    // todo
+
+    /* Redemption */
+    // todo
+
+    /* Repentance */
+    // todo
+
+    /* Repentance */
+    // todo
+
+    /* Rooted */
+    // todo
+
+    /* Rooted */
+    // todo
+
+    /* SI:7 Agent */
+    // todo
+
+    /* Savagery */
+    // todo
+
+    /* Savannah Highmane */
+    // todo
+
+    /* Scarlet Crusader */
+    // todo
+
+    /* Scavenging Hyena */
+    // todo
+
+    /* Sea Giant */
+    // todo
+
+    /* Secretkeeper */
+    // todo
+
+    /* Sense Demons */
+    // todo
+
+    /* Shadow Madness */
+    // todo
+
+    /* Shadow Madness */
+    // todo
+
+    /* Shadow of Nothing */
+    // todo
+
+    /* Shadowflame */
+    // todo
+
+    /* Shadowform */
+    // todo
+
+    /* Shadows of M'uru */
+    // todo
+
+    /* Shadowstep */
+    // todo
+
+    /* Shan'do's Lesson */
+    // todo
+
+    /* Sharp! */
+    // todo
+
+    /* Shield Slam */
+    // todo
+
+    /* Shieldbearer */
+    // todo
+
+    /* Silence */
+    // todo
+
+    /* Silver Hand Knight */
+    // todo
+
+    /* Silvermoon Guardian */
+    // todo
+
+    /* Siphon Soul */
+    // todo
+
+    /* Slam */
+    // todo
+
+    /* Snake */
+    // todo
+
+    /* Snake Trap */
+    // todo
+
+    /* Snipe */
+    // todo
+
+    /* Sorcerer's Apprentice */
+    // todo
+
+    /* Soul of the Forest */
+    // todo
+
+    /* Soul of the Forest */
+    // todo
+
+    /* Southsea Captain */
+    // todo
+
+    /* Southsea Deckhand */
+    // todo
+
+    /* Spellbender */
+    // todo
+
+    /* Spellbender */
+    // todo
+
+    /* Spellbreaker */
+    // todo
+
+    /* Spirit Wolf */
+    // todo
+
+    /* Spiteful Smith */
+    // todo
+
+    /* Squire */
+    // todo
+
+    /* Squirrel */
+    // todo
+
+    /* Stampeding Kodo */
+    // todo
+
+    /* Stand Down! */
+    // todo
+
+    /* Starfall */
+    // todo
+
+    /* Starfall */
+    // todo
+
+    /* Starfall */
+    // todo
+
+    /* Stormforged Axe */
+    // todo
+
+    /* Stranglethorn Tiger */
+    // todo
+
+    /* Strength of the Pack */
+    // todo
+
+    /* Summon a Panther */
+    // todo
+
+    /* Summoning Portal */
+    // todo
+
+    /* Sunfury Protector */
+    // todo
+
+    /* Sunwalker */
+    // todo
+
+    /* Sword of Justice */
+    // todo
+
+    /* Sylvanas Windrunner */
+    // todo
+
+    /* Tauren Warrior */
+    // todo
+
+    /* Teachings of the Kirin Tor */
+    // todo
+
+    /* Tempered */
+    // todo
+
+    /* Temple Enforcer */
+    // todo
+
+    /* The Beast */
+    // todo
+
+    /* The Black Knight */
+    // todo
+
+    /* Thoughtsteal */
+    // todo
+
+    /* Thrallmar Farseer */
+    // todo
+
+    /* Tinkmaster Overspark */
+    // todo
+
+    /* Tirion Fordring */
+    // todo
+
+    /* Trapped */
+    // todo
+
+    /* Treant */
+    // todo
+
+    /* Treant */
+    // todo
+
+    /* Treant */
+    // todo
+
+    /* Treasure Crazed */
+    // todo
+
+    /* Twilight Drake */
+    // todo
+
+    /* Twisting Nether */
+    // todo
+
+    /* Unbound Elemental */
+    // todo
+
+    /* Unleash the Hounds */
+    // todo
+
+    /* Upgrade! */
+    // todo
+
+    /* Upgraded */
+    // todo
+
+    /* Upgraded */
+    // todo
+
+    /* Uproot */
+    // todo
+
+    /* Uprooted */
+    // todo
+
+    /* VanCleef's Vengeance */
+    // todo
+
+    /* Vaporize */
+    // todo
+
+    /* Venture Co. Mercenary */
+    // todo
+
+    /* Violet Apprentice */
+    // todo
+
+    /* Violet Teacher */
+    // todo
+
+    /* Void Terror */
+    // todo
+
+    /* Warded */
+    // todo
+
+    /* Well Fed */
+    // todo
+
+    /* Whelp */
+    // todo
+
+    /* Whelp */
+    // todo
+
+    /* Whipped Into Shape */
+    // todo
+
+    /* Wild Pyromancer */
+    // todo
+
+    /* Windfury Harpy */
+    // todo
+
+    /* Wisp */
+    // todo
+
+    /* Worgen Infiltrator */
+    // todo
+
+    /* Worthless Imp */
+    // todo
+
+    /* Wrath */
+    // todo
+
+    /* Wrath */
+    // todo
+
+    /* Wrath */
+    // todo
+
+    /* Yarrr! */
+    // todo
+
+    /* Young Dragonhawk */
+    // todo
+
+    /* Young Priestess */
+    // todo
+
+    /* Youthful Brewmaster */
+    // todo
+
+    /* Ysera */
+    // todo
+
+    /* Ysera Awakens */
+    // todo
 
 }
