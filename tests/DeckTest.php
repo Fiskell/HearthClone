ea5f76981ab87109f893032336169381cf38e9cc
Fiskell <stephen.fiskell@gmail.com>
2015-10-17 18:44:15 -0700

    Load deck from file working successfully

--- tests/DeckTest.php
+++ tests/DeckTest.php
@@ -12,12 +12,19 @@ class DeckTest extends HearthCloneTest
     }
 
     public function test_deck_list_is_loaded_on_initialization() {
-        $hunter_deck_json = json_decode(file_get_contents(base_path() . "/resources/deck_lists/basic_only_hunter.json"));
-        $priest_deck_json = json_decode(file_get_contents(base_path() . "/resources/deck_lists/basic_only_priest.json"));
-        $player1_deck = app('Deck', [app(HeroClass::$HUNTER, [$this->game->getPlayer1()]), array_get($hunter_deck_json, [])]);
-        $player2_deck = app('Deck', [app(HeroClass::$PRIEST, [$this->game->getPlayer2()]), array_get($priest_deck_json, [])]);
+//        $hunter_deck_json = json_decode(file_get_contents(base_path() . "/resources/deck_lists/basic_only_hunter.json"));
+//        $priest_deck_json = json_decode(file_get_contents(base_path() . "/resources/deck_lists/basic_only_priest.json"));
+//        $player1_deck = app('Deck', [app(HeroClass::$HUNTER, [$this->game->getPlayer1()]), array_get($hunter_deck_json, [])]);
+//        $player2_deck = app('Deck', [app(HeroClass::$PRIEST, [$this->game->getPlayer2()]), array_get($priest_deck_json, [])]);
+//
+//        $this->game->init($player1_deck, $player2_deck);
+//        $this->assertEquals(30, $this->game->getPlayer1()->getDeck()->getRemainingCount());
+    }
 
-        $this->game->init($player1_deck, $player2_deck);
-        $this->assertEquals(30, $this->game->getPlayer1()->getDeck()->getRemainingCount());
+    public function test_single_deck_load() {
+        $hunter_deck_json = file_get_contents(base_path() . "/resources/deck_lists/basic_only_hunter.json");
+        $hero = app(HeroClass::$HUNTER, [$this->game->getPlayer1()]);
+        $cards = array_get(json_decode($hunter_deck_json, true), 'Cards', []);
+        $player1_deck = app('Deck', [$hero, $cards]);
     }
 }
